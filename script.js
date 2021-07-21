29cecb7b9228bde48e8efdbac4c521b3351d6784
BinSophaneth <81245375+BinSophaneth@users.noreply.github.com>
2021-07-21 23:50:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,30 @@
 let currentDay = null;
-// let events = null;
-// $(function () {
-//   $('input[id="input-date"]').daterangepicker({
-//     autoUpdateInput: false,
-//     locale: {
-//       cancelLabel: "Clear",
-//     },
-//   });
-
-//   $('input[id="input-date"]').on(
-//     "apply.daterangepicker",
-//     function (ev, picker) {
-//       $(this).val(
-//         picker.startDate.format("DD-MMMM-YYYY") +
-//           " --> " +
-//           picker.endDate.format("DD-MMMM-YYYY")
-//       );
-//     }
-//   );
-// });
-// $('document').on('focus',"#input-date", function(){
-//   $(this).daterangepicker();
-// });​
-$(function () {
-  $('input[id="start-date"]').daterangepicker({
-    locale: {
-      format: "DD/MM/YYYY",
-      cancelLabel: "Clear",
-    },
-    singleDatePicker: true,
-    minDate: "29/03/2020",
-    maxDate: "02/05/2020",
-  });
-  $('input[id="start-date"]').on(
-    "cancel.daterangepicker",
-    function (ev, picker) {
-      $(this).val("");
-    }
-  );
-});
-$(function () {
-  $('input[id="end-date"]').daterangepicker({
-    locale: {
-      format: "DD/MM/YYYY",
-      cancelLabel: "Clear",
-    },
-    singleDatePicker: true,
-    minDate: "29/03/2020",
-    maxDate: "02/05/2020",
-  });
-  $('input[id="end-date"]').on("cancel.daterangepicker", function (ev, picker) {
-    $(this).val("");
-  });
-});
-
 $("#exampleModal").on("hidden.bs.modal", function () {
   $("#myform").trigger("reset");
 });
 $("#myform").submit(function (event) {
   event.preventDefault();
-var range = [];
+  let startdate = moment($("#start-date").val(), "DD/MM/YYYY");
+  let enddate = moment($("#end-date").val(), "DD/MM/YYYY");
+  let countdate = enddate.diff(startdate, "days");
+  // console.log(enddate.diff(startdate, "days"));
+  // console.log(a.diff(b, 'days'));
+  var range = [];
+  if (startdate == enddate) {
+    range.push(startdate.format("DD/MM/YYYY"));
+  } else {
+    for (var i = 0; i < countdate + 1; i++) {
+      range.push(startdate.format("DD/MM/YYYY"));
+      startdate.add(1, "days");
+    }
+  }
+  for (var i = 0; i < range.length; i++) {
+    console.log(range[i]);
+    $("div[date='" + range[i] + "']").append(
+      '<input type="text" class="input-name color1" placeholder="Edit here" />'
+    );
+  }
+  // console.log(range[0]);
   // let tmpObj = {
   //   title: $("#inputTitle").val(),
   //   type: $("#inputGroupSelect").val(),
@@ -75,6 +40,20 @@ $(".date-container").click(function () {
   $("#start-date").val(startDate);
   let endDate = $(this).children(0).attr("date");
   $("#end-date").val(endDate);
+  $('input[name="daterange"]').daterangepicker({
+    locale: {
+      format: "DD/MM/YYYY",
+      cancelLabel: "Clear",
+    },
+    singleDatePicker: true,
+    autoUpdateInput: true,
+    minDate: "29/03/2020",
+    maxDate: "02/05/2020",
+  });
+  $('input[id="end-date"]').on("cancel.daterangepicker", function (ev, picker) {
+    $(this).val("");
+  });
+
   currentDay = passedID;
   let data = sessionStorage.getItem("29/03/2020");
   if (data !== null) {
